2ef4060afbb04722c92fa5f2006940a2b382ac47
VINICIUS ALMEIDA DE SOUZA <v.almsou@uol.com.br>
2020-09-20 15:32:20 -0300

    formulario

---
+++ form.js
@@ -0,0 +1,33 @@
+(() => {
+    const abas = document.querySelectorAll('[data-aba]') (parameter) aba: Element
+
+    esconderConteudos =() => {
+        const contudos = document.querySelectorAll('[data-conteudos]')
+        conteudos.forEach(conteudo =>conteudo.classlist.add('hide'))
+
+    }
+    
+    inativarAbas =() => {
+        abas.forEach(aba =>aba.classlist.remove('ativa'))
+        
+    }
+
+    ativarConteudo =(valor) => {
+        const conteudo = document.querySelector('[data-conteudo="${valor}"]')
+        conteudo.classlist.remove('hide')
+    }
+    
+    ativarAba =(aba) => {
+      aba.classlist,add('ativa')
+    }
+    
+
+    abas.forEach(aba => aba.addEventListener('click',() => {    const valor = aba.dataset.aba
+
+        esconderConteudos()
+        inativarAbas()
+        ativarConteudo(valor)
+        ativaraAbaO(aba)
+
+    }))
+})()
