3751eca4ec6ac3ce439011edd2886ed830d930d7
VINICIUS ALMEIDA DE SOUZA <v.almsou@uol.com.br>
2020-09-21 12:20:28 -0300

    js functions on menu

---
+++ position-menu.js
@@ -0,0 +1,24 @@
+
+const $elementEntrar = document.querySelector('.login')
+const $elementSair = document.querySelector('.logout')
+const $elementBtn = document.getElementById('btn-entrar')
+
+var popup = document.getElementById("myPopup");
+popup.classList.toggle("show");
+
+
+
+
+
+
+
+$elementBtn.addEventListener('click', function(event){
+    document.write('Olá')
+
+})
+
+/*$elementSair.addEventListener('click', function(event){
+    this.classList.toggle('elementActive')
+    $elementSair.classList.add('elementActive')
+
+})
